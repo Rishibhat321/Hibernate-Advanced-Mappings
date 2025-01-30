f3c787e345120c48ef8a272d708c40c0ec78b1bc
Rishib#321 <rishitab2209@gmail.com>
2025-01-30 13:09:07 +0530

    one-to-one-uni

--- 01-jpa-one-to-one-uni/src/main/java/com/mappings/cruddemo/entity/InstructorDetail.java
+++ 01-jpa-one-to-one-uni/src/main/java/com/mappings/cruddemo/entity/InstructorDetail.java
@@ -1,4 +1,67 @@
 package com.mappings.cruddemo.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructor_detail")
 public class InstructorDetail {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    // define constructors
+    public InstructorDetail() {
+
+    }
+
+    public InstructorDetail(String hobby, String youtubeChannel) {
+        this.hobby = hobby;
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    // define getters/setters
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    // define toString
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "hobby='" + hobby + '\'' +
+                ", id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                '}';
+    }
+
 }
